f281a9769b6debde1a04ac322db09695cdac0e1c
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-05-28 19:30:01 +0530

    Add: implement useDataStore

--- src/pages/ProblemList.tsx
+++ src/pages/ProblemList.tsx
@@ -1,41 +1,15 @@
 import { useEffect } from "react";
-import { databaseRef } from "@/Firebase";
-import { useState } from "react";
-import { getDocs, collection } from "firebase/firestore";
+
+import useDataStore from "@/zustang/useDataStore";
+
 import TableList from "@/components/Table/TableList";
 
-interface Problem {
-  id: string;
-  Title: string;
-  Statement: string;
-  Level: string;
-  Input: string;
-  Output: string;
-  Contraints: string;
-}
-
 const ProblemList = () => {
-  const [problems, setProblems] = useState<Problem[]>([]);
-  const [loading, setLoading] = useState(true);
-  console.log(loading);
-  useEffect(() => {
-    async function getProblems() {
-      try {
-        const prob = await getDocs(collection(databaseRef, "Problems"));
-        const p = prob.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        })) as Problem[];
-        setProblems(p);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
+  const { problems, loading, getProblems } = useDataStore();
 
+  useEffect(() => {
     getProblems();
-  });
+  }, []);
 
   return (
     <>
@@ -43,7 +17,9 @@ const ProblemList = () => {
         <div className="max-w-[1200px] outline outline-[1px] outline-zinc-200 rounded-lg">
           <TableList problems={problems} />
         </div>
-        <p className="text-zinc-600 text-sm font-bold pt-3">A list of your recent invoices.</p>
+        <p className="text-zinc-600 text-sm font-bold pt-3">
+          A list of your recent invoices.
+        </p>
       </div>
     </>
   );
