ff85e2e210db4290a028adb432ef58c97086a194
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-06-06 00:57:11 +0530

    Add: Nav Component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import { GoogleLoginButton } from "react-social-login-buttons";
 import { authRef, AuthProvider } from "@/Firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import useUserDataStore from "@/zustang/useUserData";
 
 const Login = () => {
-  const { setIsLoggedIn, setUserData } = useUserDataStore();
+  const { setIsLoggedIn, setUserData, isLoggedIn } = useUserDataStore();
 
   const handleLogin = async () => {
     await signInWithPopup(authRef, AuthProvider)
@@ -14,22 +14,28 @@ const Login = () => {
         setUserData({
           userEmail: res.user.email || "",
           userName: res.user.displayName || "",
-          userImageUrl: res.user.photoURL || ""
+          userImageUrl: res.user.photoURL || "",
         });
+
+        console.log(res.user.photoURL)
       })
       .catch((error) => {
         console.log(error.errorMessage);
       });
   };
 
+  console.log(isLoggedIn)
+
   return (
     <>
+ 
       <div className="w-full h-[100vh] flex justify-center items-center">
         <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
         <div className="w-[310px] shadow-2xl">
           <GoogleLoginButton onClick={handleLogin} />
         </div>
       </div>
+     
     </>
   );
 };
