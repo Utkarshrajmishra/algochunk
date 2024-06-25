cc5e82a8663360b59e674be856b146e80d1c3b55
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2024-06-26 01:43:07 +0530

    Update: Post Component

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,40 +1,45 @@
 import { FC } from "react";
+import { Timestamp } from "firebase/firestore";
+import formatPostTime from "@/utils/FormateTime";
 
-type Post = {
-  postContent: string;
-  timeStamp: string;
-  userName: string;
+interface Post {
+  id: string;
   photoUrl: string;
-};
+  userName: string;
+  userID: string;
+  postContent: string;
+  timeStamp: Timestamp;
+}
 
 interface PostProps {
   Post: Post;
 }
 
-const Post: FC<PostProps> = ({ Post }) => {
+const PostComp: FC<PostProps> = ({ Post }) => {
+  
+  const time=formatPostTime(Post.timeStamp)
+
   return (
-    <>
-      <div className=" outline h-fit outline-1 bg-neutral-900  rounded-lg p-4">
-        <div className="flex p-3 pt-0">
-          <img
-            src={Post.photoUrl}
-            alt="Profile Image"
-            className="rounded-full"
-            width={50}
-          />
-          <div className="pl-2 text-zinc-200 ">
-            <h1 className="text-zinc-200 font-semibold text-lg">
-              {Post.userName}
-            </h1>
-            <p className="text-sm">{Post.timeStamp}</p>
-          </div>
-        </div>
-        <div className="text-zinc-200 p-3 tracking-wide rounded-xl bg-neutral-800 ">
-          {Post.postContent}
+    <div className="outline h-fit outline-1 bg-neutral-900 rounded-lg p-4">
+      <div className="flex p-3 pt-0">
+        <img
+          src={Post.photoUrl}
+          alt="Profile Image"
+          className="rounded-full"
+          width={50}
+        />
+        <div className="pl-2 text-zinc-200">
+          <h1 className="text-zinc-200 font-semibold text-lg">
+            {Post.userName}
+          </h1>
+          <p className="text-sm">{time}</p>
         </div>
       </div>
-    </>
+      <div className="text-zinc-200 p-3 tracking-wide rounded-xl bg-neutral-800">
+        <pre>{Post.postContent}</pre>
+      </div>
+    </div>
   );
 };
 
-export default Post;
+export default PostComp;
